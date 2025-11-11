7b300a95247e6db67ed944cc8f2893362bc3bb4f
apponislam <11appon11@gmail.com>
2025-11-12 05:09:45 +0600

    all pages construction page created

---
+++ src/app/(dashboard)/settings/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const page = () => {
+    return (
+        <div className=" text-center">
+            <h1 className="uppercase text-5xl">This page is under construction</h1>
+        </div>
+    );
+};
+
+export default page;
