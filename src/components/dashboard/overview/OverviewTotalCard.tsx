367b02c990ab12aacdfbba2a38262e4dd7aedcb8
Appon Islam <appon2003@gmail.com>
2026-01-07 02:49:05 +0600

    new line added

--- src/components/dashboard/overview/OverviewTotalCard.tsx
+++ src/components/dashboard/overview/OverviewTotalCard.tsx
@@ -65,7 +65,7 @@ import { ArrowUp, ArrowDown } from "lucide-react";
 import Image from "next/image";
 
 const OverviewTotalCard = ({ dashboardData }: any) => {
-    // Use real data from API
+    // Use real data from API aaadsfa
     const cards = [
         {
             title: "Total job complete",
