5027e0877f612d669cc5d594b93f736e5ae9d2bd
Appon Islam <appon2003@gmail.com>
2026-01-07 07:17:41 +0600

    next config updated

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
                 hostname: "texme-media-bucket.s3.us-east-1.amazonaws.com",
                 pathname: "/**",
             },
+            {
+                protocol: "https",
+                hostname: "cdn.example.com",
+                pathname: "/**",
+            },
         ],
     },
 };
